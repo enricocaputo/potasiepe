79e5cf2e74b654f5ef9a514ef489ceb8c138b9d5
Enrico <enricocaputo.1972@gmail.com>
2019-11-15 23:19:05 +0100

    sw

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,6 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 if(workbox){
   workbox.setConfig({debug:false});
   workbox.skipWaiting();
   workbox.clientsClaim();
-  workbox.routing.registerRoute(
-  '/img/slides/',
-  new workbox.strategies.NetworkFirst({
-      networkTimeoutSeconds: 3,
-      cacheName: 'slides',
-      plugins: [
-        new workbox.expiration.Plugin({
-          maxEntries: 5,
-          maxAgeSeconds: 5 * 60, // 5 minutes
-        }),
-      ],
-  })
-);
-};
+  workbox.routing.registerRoute(new RegExp('.*'),workbox.strategies.networkFirst());};
